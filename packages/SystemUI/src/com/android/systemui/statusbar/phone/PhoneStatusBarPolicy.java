f0f422db59d413fd134bacb59a287beef635438f
LuK1337 <priv.luk@gmail.com>
2023-11-05 13:42:18 +0200

    fixup! Implement firewall status bar indicator
    Fix tests.
    
    Change-Id: Ia7adb7d77cd8d4c4074d33c312a67c7e93128ffa
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: 0xSecureByte <chirantan.code@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -190,6 +190,7 @@ public class PhoneStatusBarPolicy
 
     private BluetoothController mBluetooth;
     private AlarmManager.AlarmClockInfo mNextAlarm;
+    private final Context mContext;
 
     private NfcAdapter mAdapter;
     private final Context mContext;
